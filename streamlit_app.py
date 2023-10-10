533df806c8164a9aef17e1a9673de8e6b6e51bad
kitouni <28654657+okitouni@users.noreply.github.com>
2023-10-10 18:19:41 -0400

    3D plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,30 +5,40 @@ import streamlit as st
 
 # Generate data and PCA
 df = pd.read_csv("pca.csv")
-zn = df[["z", "n"]].values
 
-fig = go.Figure()
-
-
-# colors = embs_pca[:, 2]
 st.title("Interactive PCA Plot with Streamlit")
 
 # Sliders for selecting PCs
-
+st.write("## 2D PCA Plot")
 x_pc = st.slider("Select PC for X-axis:", 1, 10, 1)
 y_pc = st.slider("Select PC for Y-axis:", 1, 10, 2)
 color = st.slider("Select PC for color:", 1, 10, 3)
 
 
 fig = px.scatter(
-        df,
-        x=f"PC{x_pc}",
-        y=f"PC{y_pc}",
-        hover_data=["z", "n"],
-        labels={"x": f"PC{x_pc}", "y": f"PC{y_pc}"},
-        color=f"PC{color}",
-        color_continuous_scale="rdbu"
-    )
+    df,
+    x=f"PC{x_pc}",
+    y=f"PC{y_pc}",
+    hover_data=["z", "n"],
+    labels={"x": f"PC{x_pc}", "y": f"PC{y_pc}"},
+    color=f"PC{color}",
+    color_continuous_scale="rdbu",
+)
 
 st.plotly_chart(fig)
 
+# add another plot but in 3D, with the same sliders
+st.write("## 3D PCA Plot")
+z_pc = st.slider("Select PC for Z-axis:", 1, 10, 3)
+
+fig = px.scatter_3d(
+    df,
+    x=f"PC{x_pc}",
+    y=f"PC{y_pc}",
+    z=f"PC{z_pc}",
+    hover_data=["z", "n"],
+    color=f"PC{color}",
+    color_continuous_scale="rdbu",
+)
+
+st.plotly_chart(fig)
